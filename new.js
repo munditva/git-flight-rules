0ff8c4d3ec6444215c914c5e2f619a14b872154c
Tushar <tusm851@gmail.com>
2020-12-06 01:44:41 +0530

    Initial commit

---
+++ new.js
@@ -0,0 +1,8 @@
+alert("Hey there! I am the JS Stalker");
+var fname=prompt("Your First name");
+var sname=prompt("Your Last name");
+var age=prompt("Your Age");
+alert("Your full name is" + fname+ sname);
+alert("ANd, you are " + age+" years old");
+document.write("YO Baby");
+
